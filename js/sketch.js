8a06f06dbd672a0b00ecc756a7b4448c4fb227fe
chrisPhat <christopher.booth@plymouth.ac.uk>
2022-10-31 17:01:23 +0000

    Added shapes and fills for PS Icons

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,40 @@
 function setup() {
-    var canvas = createCanvas(800, 800);
+    var canvas = createCanvas(600, 600);
     canvas.parent("p5container");
 }
 
 function draw() {
-    background(220);
+    background('#d9d9d9');
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    noStroke();
+
+    //Button Backgrounds
+    fill('#383838');
+    ellipse(150, 150, 250);
+    ellipse(150, 450, 250);
+    ellipse(450, 150, 250);
+    ellipse(450, 450, 250);
+
+    noFill();
+    strokeWeight(20);
+
+    //Triangle
+    stroke('#38DE9C')
+    triangle(150, 80, 220, 200, 80, 200);
+
+    //Sqaure
+    stroke('#DF9ACA');
+    rect(385, 85, 130);
+
+    //Cross
+    stroke('#6BBAC8');
+    strokeCap(SQUARE);
+    line(95, 385, 210, 515);
+    line(95, 515, 210, 385);
+
+    //Circle
+    stroke('#f52c31');
+    ellipse(450, 450, 180);
+
+    
 }
